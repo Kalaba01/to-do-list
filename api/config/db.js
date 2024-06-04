ad9a9d532dae5545b43b84824255198b4c75db37
Kalaba <edinkalaba45@gmail.com>
2024-06-04 15:01:52 +0200

    Dodat .env fajl i updatan .gitignore fajl

--- api/config/db.js
+++ api/config/db.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
+// require('dotenv').config(); - ENV file import
 
 module.exports = {
   connectToDb: async () => {
     try {
-      await mongoose.connect('mongodb+srv://edinkalaba45:Pw9OCeKYB2iOUWXv@todo.wru78ea.mongodb.net/todo?retryWrites=true&w=majority');
+      await mongoose.connect('mongodb+srv://edinkalaba45:Pw9OCeKYB2iOUWXv@todo.wru78ea.mongodb.net/todo?retryWrites=true&w=majority'); // URL was not changed to process.env.MONGO_URL because testing purposes
     } catch (err) {
       console.error('Connection error', err.message);
       throw err;

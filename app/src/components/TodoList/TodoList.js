e52ff2c87c9ce278c417a9804848cd4e6d74465c
Kalaba <edinkalaba45@gmail.com>
2024-06-05 15:02:48 +0200

    Updatan dark mode

--- app/src/components/TodoList/TodoList.js
+++ app/src/components/TodoList/TodoList.js
@@ -3,6 +3,7 @@
 import React, { useState, useEffect } from 'react';
 import { TodoCard, TodoEdit } from "../index";
 import { FaShare } from "react-icons/fa";
+import { MdOutlineAttachEmail } from "react-icons/md";
 import { MdDelete } from "react-icons/md";
 import "./TodoList.css";
 
@@ -37,7 +38,7 @@ const TodoList = ({ todos, deleteTodo, deleteAllTodos, readTask, editTodo, upgra
   return (
     <div className={`TodoList ${todos.length === 0 ? 'hidden' : ''}`}>
       <div className="icons">
-        <span><FaShare onClick={()=>{shareTodos()}} size={20} /></span>
+        <span><MdOutlineAttachEmail onClick={()=>{shareTodos()}} size={20} /></span>
         <span><MdDelete size={20} onClick={()=>deleteAllTodos()} /></span>
       </div>
       <div className="filter-options">
